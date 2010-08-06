b096972ca7023d01664b9a311366df6631dc2675
Joel Oliveira <Joel@Chunk.local>
2010-08-05 21:18:56 -0400

    index.haml begun. visual style cues taken from radiant-prototype.  sass and js files created and started

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -1,3 +1,14 @@
 class Admin::IssuesController < Admin::ResourceController
   model_class Issue
+  before_filter :add_assets
+  
+protected
+
+  def add_assets
+    include_javascript("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js")
+    include_javascript("/javascripts/issue_tracker.js")
+    include_stylesheet("/stylesheets/issue_tracker.css")
+    include_stylesheet("/stylesheets/issue_table.css")
+  end
+  
 end
